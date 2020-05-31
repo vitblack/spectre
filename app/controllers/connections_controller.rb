034d32e084c71ca4eddbeb1c78fef2c0e6d92e33
VitalyB <vitaly.innocoding@gmail.com>
2020-05-31 20:32:38 +0300

    create connections

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,5 +3,16 @@
 class ConnectionsController < DashboardController
   def index; end
 
-  def new; end
+  def new
+    result = Connections::CreateService.call(user: current_user)
+
+    if result.success?
+      connect_url = result.connect_url
+
+      redirect_to connect_url
+    else
+      flash[:alert] = result.error
+      redirect_back fallback_location: root_path
+    end
+  end
 end
