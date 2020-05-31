528725881ec182ac8017362e18bfe4dc5da71edf
VitalyB <vitaly.innocoding@gmail.com>
2020-05-31 23:35:07 +0300

    diplay transactions count on accounts table

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 module PagesHelper
+  def transactions_count(account)
+    transactions_count = account[:extra][:transactions_count]
+
+    return 0 if transactions_count.blank?
+
+    posted  = transactions_count[:posted].to_i
+    pending = transactions_count[:pending].to_i
+    total   = posted + pending
+
+    return 0 if total.zero?
+
+    link_to(total, connection_transactions_path(account[:connection_id], account_id: account[:id]))
+  end
 end
