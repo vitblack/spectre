ba90ca4796f041a2c3c21515ec540386ab51ba1b
VitalyB <vitaly.innocoding@gmail.com>
2020-06-01 02:49:44 +0300

    add identification service

--- app/controllers/api/callbacks_controller.rb
+++ app/controllers/api/callbacks_controller.rb
@@ -2,10 +2,21 @@
 
 module Api
   class CallbacksController < ApiController
+    before_action :authenticate
+
     def success
+      head 204
     end
 
     def fail
+      head 204
+    end
+
+    private
+
+    def authenticate
+      result = IdentificationService.call(request: request)
+      head 401 if result.failure?
     end
   end
 end
