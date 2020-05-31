fb0cea30de60d92dbde7fb26cfed868dcc1a3fe5
VitalyB <vitaly.innocoding@gmail.com>
2020-06-01 00:21:13 +0300

    fetch accounts by connection_id

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,6 +2,12 @@
 
 class AccountsController < DashboardController
   def index
+    result = Accounts::ListService.call(user: current_user, connection_id: params[:connection_id])
 
+    if result.success?
+      @accounts = result.accounts
+    else
+      flash[:alert] = result.error
+    end
   end
 end
